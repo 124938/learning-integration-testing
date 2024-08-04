6b44720560a64f56aa2c3e286984438485e16267
Shreyashkumar Limbhetwala <shrey.9129@gmail.com>
2024-08-04 22:49:01 +0530

    Initial push

---
+++ src/main/java/com/shrey/learning/learning_integration_testing/LearningIntegrationTestingApplication.java
@@ -0,0 +1,13 @@
+package com.shrey.learning.learning_integration_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningIntegrationTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningIntegrationTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shrey/learning/learning_integration_testing/LearningIntegrationTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shrey.learning.learning_integration_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningIntegrationTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
