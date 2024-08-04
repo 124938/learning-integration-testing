76f78f4b6d45a337cae2cd874f7f8c97ee41afe4
Shreyashkumar Limbhetwala <shrey.9129@gmail.com>
2024-08-04 23:15:10 +0530

    SQL script  with create table and sample data

--- src/test/java/com/shrey/learning/learning_integration_testing/LearningIntegrationTestingApplicationTests.java
+++ src/test/java/com/shrey/learning/learning_integration_testing/LearningIntegrationTestingApplicationTests.java
@@ -1,13 +1,31 @@
 package com.shrey.learning.learning_integration_testing;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlMergeMode;
+
+import javax.sql.DataSource;
+
+import static org.springframework.test.context.jdbc.SqlMergeMode.MergeMode.MERGE;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("it")
+@SqlMergeMode(MERGE)
+@Sql(scripts = {"classpath:sql/it-init.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 class LearningIntegrationTestingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    @Sql("classpath:sql/it-sample-data.sql")
+    public void contextLoads() {
+
+    }
 
 }
